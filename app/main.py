1123263a5bc05332d784fdf3919beeeac71f7697
brilbrilbril <maghatan@gmail.com>
2024-11-30 17:13:11 +0700

    debugging the main.py

--- app/main.py
+++ app/main.py
@@ -6,25 +6,27 @@ from transformers import pipeline
 
 app = FastAPI()
 
-generator = pipeline('text-generation', model='gpt2')
+# generator = pipeline('text-generation', model='gpt2')
 
-# Define a Pydantic model for the request body
-class GenerateRequest(BaseModel):
-    processed_data: str
+# # Define a Pydantic model for the request body
+# class GenerateRequest(BaseModel):
+#     processed_data: str
 
-@app.post("/generate")
-def generate_text(request: GenerateRequest):
-    result = generator(request.processed_data, max_length=50, num_return_sequences=1)
-    return {"generated_text": result[0]['generated_text']}
+# @app.post("/generate")
+# def generate_text(request: GenerateRequest):
+#     result = generator(request.processed_data, max_length=50, num_return_sequences=1)
+#     return {"generated_text": result[0]['generated_text']}
 
 @app.get("/")
 def read_root():
     return {"Hello": "Fastapi test model in container"}
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import uvicorn
+#     port = int(os.environ.get("PORT", 8080))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
+
+
 
 # app = Flask(__name__)
 # generator = pipeline('text-generation', model='gpt2')
