c5ae53b4c03b5142ac909a31af444afe4f2ce644
brilbrilbril <maghatan@gmail.com>
2024-11-30 16:54:20 +0700

    add if__name__ in main.py

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,5 @@ RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 COPY ./app /app/app
 
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "$PORT"]
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
 #from flask import Flask, request, jsonify
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
+
+app = FastAPI()
+
+generator = pipeline('text-generation', model='gpt2')
+
+# Define a Pydantic model for the request body
+class GenerateRequest(BaseModel):
+    processed_data: str
+
+@app.post("/generate")
+def generate_text(request: GenerateRequest):
+    result = generator(request.processed_data, max_length=50, num_return_sequences=1)
+    return {"generated_text": result[0]['generated_text']}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Fastapi test model in container"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # app = Flask(__name__)
 # generator = pipeline('text-generation', model='gpt2')
@@ -16,23 +39,3 @@ from transformers import pipeline
 #     app.run(host='0.0.0.0', port=8080)
 
 # Initialize the GPT-2 model and tokenizer
-
-app = FastAPI()
-
-generator = pipeline('text-generation', model='gpt2')
-
-# Define a Pydantic model for the request body
-class GenerateRequest(BaseModel):
-    processed_data: str
-
-# Define the API endpoint for text generation
-@app.post("/generate")
-def generate_text(request: GenerateRequest):
-    result = generator(request.processed_data, max_length=50, num_return_sequences=1)
-    return {"generated_text": result[0]['generated_text']}
-
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "Fastapi test model in container"}
